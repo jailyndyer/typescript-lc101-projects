317c1fcd9fb41dd49ed7e084cf504332766eae51
unknown <jdyerks@gmail.com>
2021-08-05 19:52:33 -0500

    Studio complete

---
+++ studio/Rocket.ts
@@ -0,0 +1,50 @@
+import { Payload } from './Payload';
+import { Cargo } from './Cargo';
+import { Astronaut } from './Astronaut';
+export class Rocket {
+	//properties and methods
+	name: string;
+	totalCapacityKg: number;
+	cargoItems: Cargo[]=[];
+	astronauts: Astronaut[]=[];
+	constructor(name: string, totalCapacityKg: number) {
+		this.name = name;
+		this.totalCapacityKg = totalCapacityKg;
+	}
+	
+	sumMass(items:Payload[]): number {
+		let itemsMass = 0;
+	
+		for (let i=0; i<items.length; i++){
+			itemsMass = itemsMass + items[0].massKg;
+		}
+	
+		return itemsMass;
+	}
+	
+	currentMassKg(): number {
+			return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+	}
+	
+	canAdd(item: Payload): boolean {
+			return (this.currentMassKg() + item.massKg) < this.totalCapacityKg;
+	}
+	
+	addCargo(cargo: Cargo): boolean {
+		if (this.canAdd(cargo)){
+			this.cargoItems.push(cargo);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	addAstronaut(astronaut: Astronaut): boolean {
+		if (this.canAdd(astronaut)){
+			this.astronauts.push(astronaut);
+			return true;
+		}
+		
+		return false;
+	}			
+}
